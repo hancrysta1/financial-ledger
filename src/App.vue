969386b1f8440c4b522e38f00b6bea0da7446f2d
hancrysta1 <hancrystal@sookmyung.ac.kr>
2024-06-14 10:02:11 +0900

    Feat: 헤더 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
-<script setup>
-</script>
-
 <template>
   <div>
     <RouterView />
   </div>
+  <div id="home">
+    <div class="container">
+      <Header />
+      <GridContainer/>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import Header from './components/Header.vue';
+import GridContainer from './components/GridContainer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    GridContainer
+  }
+}
+</script>
+
+<style>
+@font-face {
+  font-family:'NanumSquareRoundEB';
+  src: url('./assets/fonts/NanumSquareRoundEB.ttf');
+}
+@font-face {
+  font-family:'NanumSquareRoundB';
+  src: url('./assets/fonts/NanumSquareRoundB.ttf');
+}
+
+@font-face {
+  font-family:'BlackHanSans';
+  src: url('./assets/fonts/BlackHanSans.ttf') format('truetype');
+}
+
+.container{
+  font-family: 'BlackHanSans'
+}
 </style>
---
+++ src/components/Metric.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="metric">
+      <p>
+        <span class="title">{{ title }}</span>
+        <span class="number">{{ number }}</span>
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: String,
+    number: String
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .metric {
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    padding-right: 5%;
+    padding-left: 5%;
+    margin: 5%;
+    background-color: white;
+    border: 3px solid #dce6eb;
+  }
+  .metric p {
+    margin-bottom: 0;
+    line-height: 1.2;
+    text-align: right;
+  }
+  .metric .number {
+    display: block;
+    font-size: 30px;
+    font-weight: 700;
+  }
+  .metric .title {
+    font-size: 20px;
+  }
+  </style>
+  
